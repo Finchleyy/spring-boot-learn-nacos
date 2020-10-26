ac55936c5f03c244ea609d8c79e8393df3004c74
yupengwu <384556909@qq.com>
2020-10-26 15:10:49 +0800

    nacosConfig相关配置不生效的配置修改

--- nacos-simple-consumer/src/main/java/com/ypw/nacos/simple/TestController.java
+++ nacos-simple-consumer/src/main/java/com/ypw/nacos/simple/TestController.java
@@ -1,8 +1,5 @@
 package com.ypw.nacos.simple;
 
-import com.alibaba.nacos.api.config.ConfigType;
-import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
-import com.alibaba.nacos.api.config.annotation.NacosValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
